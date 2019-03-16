97ed1176c7129c303694df5f65fe976851663165
Akio Ohta <drunkars.p@gmail.com>
2019-03-17 02:12:41 +0900

    Update download_sample_model.sh

--- tornado/models/download_sample_model.sh
+++ tornado/models/download_sample_model.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://dl.dropboxusercontent.com/u/15715059/japanese_talk_api_models/japanese_talk_api.chainermodel
-wget https://dl.dropboxusercontent.com/u/15715059/japanese_talk_api_models/vocab.bin
+wget https://www.dropbox.com/s/xtia11uvxkx0c8y/japanese_talk_api.chainermodel?dl=0
+wget https://www.dropbox.com/s/nxsbuvd4152k2di/vocab.bin?dl=0
